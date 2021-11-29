7a4fd94302d2866a121beda045747f9f1304e368
Raymond <rayho94@gmail.com>
2021-11-29 14:41:43 +0100

    Initial commit

---
+++ src/main/java/rayguru/springframework/artifactnamemsscbrewery/ArtifactNameMsscBreweryApplication.java
@@ -0,0 +1,13 @@
+package rayguru.springframework.artifactnamemsscbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactNameMsscBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactNameMsscBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rayguru/springframework/artifactnamemsscbrewery/ArtifactNameMsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package rayguru.springframework.artifactnamemsscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactNameMsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
